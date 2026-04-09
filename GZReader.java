4ab29d1fdc1122385387005c887902078698a054
Bahar1701 <baazmi2@gmail.com>
2026-04-09 14:35:11 -0400

    Update GZReader.java

--- GZReader.java
+++ GZReader.java
@@ -2,10 +2,13 @@ import java.io.*;
 import java.util.zip.GZIPInputStream;
 
 public class GZReader {
-
+    //returns a BufferedReader for reading a .gz (compressed) file
     public static BufferedReader getReader(String filePath) throws IOException {
         return new BufferedReader(
+                //convrts byte stream into character stream
                 new InputStreamReader(
+                        //decompresses the .gz file while reading
+                        //reads raw bytes from the file path
                         new GZIPInputStream(new FileInputStream(filePath))
                 )
         );
