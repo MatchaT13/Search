3a2809e8b09cbe8bcfdd3419bee31bd113bf7e01
Bahar1701 <baazmi2@gmail.com>
2026-04-09 15:26:45 -0400

    Update ResultWriter.java

--- ResultWriter.java
+++ ResultWriter.java
@@ -2,20 +2,24 @@ import java.io.*;
 import java.util.*;
 
 public class ResultWriter {
-
+    //writes ranked search results to a file in standard format
     public static void write(String fileName, List<Result> results, String groupId) throws IOException {
-
+        //create writer to output results to file
         BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
-
+        //loop through each result object
         for (Result r : results) {
+            //write result in format:
+            //queryId Q0 docId rank score groupId
+            //Q0 is a standard placeholder used in evaluation system
             writer.write(r.queryId + " Q0 " +
                     r.docId + " " +
                     r.rank + " " +
                     r.score + " " +
                     groupId);
+            //move to next line after each result
             writer.newLine();
         }
-
+        //close writer to save and release file resources
         writer.close();
     }
 }
