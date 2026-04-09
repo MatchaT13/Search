1429b6cd59c0fd79e85bf4cce48e5c84b705a51f
Bahar1701 <baazmi2@gmail.com>
2026-04-09 14:53:53 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,41 +4,44 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-
+        //root directory containing dataset (folders with .gz files)
         String dataDir = "data";   // ROOT directory
-        
+        //file containing queries (for later searching)
         String queryFile = "topics.txt";
-
+        //create indexer instance to build inverted index
         Indexer indexer = new Indexer();
-
+        
         // Traverse all folders + gz files
         List<File> gzFiles = getAllGZFiles(new File(dataDir));
-
+        //loop through each compressed file
         for (File gzFile : gzFiles) {
             System.out.println("Processing: " + gzFile.getPath());
-
+            //open .gz file using GZReader (decompression handled inside)
             BufferedReader reader = GZReader.getReader(gzFile.getPath());
+            //buffer to accumulate lines of a single document
             StringBuilder docBuffer = new StringBuilder();
             String line;
-
+            //read file line by line
             while ((line = reader.readLine()) != null) {
-
+                //start of a document
                 if (line.contains("<DOC>")) {
-                    docBuffer = new StringBuilder();
+                    docBuffer = new StringBuilder(); //reset buffer
                 }
-
+                //append current line to document buffer
                 docBuffer.append(line).append("\n");
-
+                //end of a document
                 if (line.contains("</DOC>")) {
+                    //convert accumulated lines into full document string
                     String doc = docBuffer.toString();
-
+                    //extract document ID (eg, DOCNO)
                     String docId = Parser.extractDocId(doc);
+                    //extract main text content from document
                     String text = Parser.extractText(doc);
-
+                    //index the document (build inverted index)
                     indexer.indexDocument(docId, text);
                 }
             }
-
+            //close the reader afte processing the file
             reader.close();
         }
 
@@ -78,16 +81,16 @@ public class Main {
     public static List<File> getAllGZFiles(File dir) {
 
         List<File> gzFiles = new ArrayList<>();
-
+        
         File[] files = dir.listFiles();
         if (files == null) return gzFiles;
-
+        //loop through all files
         for (File file : files) {
-
+            //if directory, recursively search inside
             if (file.isDirectory()) {
                 gzFiles.addAll(getAllGZFiles(file)); // recursion
             }
-
+            //if file ends with .gz, add it to the list
             else if (file.getName().endsWith(".GZ")) {
                 gzFiles.add(file);
             }
